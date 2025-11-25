6b104e33ef6a445a694a7ec57b0b8cfa62be65f0
Ubuntu <azureuser@Anjana-Linux-VM2.avmyxydaoftehh1k4xefuaksuf.xx.internal.cloudapp.net>
2025-11-25 20:57:05 +0000

    Add OSS-Fuzz integration files to challenge repo

---
+++ contrib/oss-fuzz/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eu
+
+# Compile dvcp.c with FUZZING_BUILD flag to exclude main()
+$CC $CFLAGS -DFUZZING_BUILD -c $SRC/dvcp/dvcp.c -o $SRC/dvcp.o
+
+# Build the fuzzer and link with dvcp.o
+$CC $CFLAGS $LIB_FUZZING_ENGINE \
+    -I$SRC/dvcp \
+    $SRC/dvcp_fuzz_linked.c \
+    $SRC/dvcp.o \
+    -o $OUT/dvcp_fuzz
+
+# Create seed corpus
+mkdir -p $OUT/dvcp_fuzz_seed_corpus
+echo -ne "IMG\x00\x01\x00\x00\x00\x01\x00\x00\x00AAAAAAAAAA" > $OUT/dvcp_fuzz_seed_corpus/seed1
